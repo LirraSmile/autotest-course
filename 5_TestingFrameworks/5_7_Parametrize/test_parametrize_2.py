deaa26059ec3a383996f92d1292c65be0c1bf45f
LirraSmile <nad.zyryanova@gmail.com>
2019-12-14 16:38:21 +0100

    Modify test_parametrize_2.py

--- 5_TestingFrameworks/5_7_Parametrize/test_parametrize_2.py
+++ 5_TestingFrameworks/5_7_Parametrize/test_parametrize_2.py
@@ -3,22 +3,20 @@ from selenium import webdriver
 import math
 import time
 
-
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
-#
 
 @pytest.mark.parametrize('number', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
 def test_should_see_hint(browser, number):
     link = f"https://stepik.org/lesson/{number}/step/1"
     browser.get(link)
-    time.sleep(10)
+
     answer_field = browser.find_element_by_class_name("textarea")
     answer = str(math.log(int(time.time())))
     answer_field.send_keys(answer)
@@ -26,10 +24,6 @@ def test_should_see_hint(browser, number):
     button = browser.find_element_by_class_name("submit-submission")
     button.click()
 
-    time.sleep(3)
-
-    expected_result = "Correct!"
     real_result_element = browser.find_element_by_class_name("smart-hints__hint")
-    real_result = real_result_element.text
 
-    assert real_result == expected_result, "Result is not correct"
+    assert "Correct!" in real_result_element.text, "Result is not correct"
